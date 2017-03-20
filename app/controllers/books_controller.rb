0dd9454f859fb2a4803f668bfb98a7a68d872903
Dan Roberts <dan@adadevelopersacademy.org>
2017-03-20 16:02:53 -0700

    Add the show method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
+  BOOKS = [{ title: "Hidden Figures", author: "Margot Lee Shetterly"},
+    { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+    { title: "Kindred", author: "Octavia E. Butler"}]
+
   def index
-    @books = [{ title: "Hidden Figures", author: "Margot Lee Shetterly"},
-              { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-              { title: "Kindred", author: "Octavia E. Butler"}]
+    @books = BOOKS
+  end
+
+  def show
+    id = params[:id].to_i
+    @book = BOOKS[id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'books', to: 'books#index'
+
+  get 'books/:id', to: 'books#show', as: 'book'
 end
