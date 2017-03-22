2e245399350f4765522fd4b16ea8a8b7160aa464
Dan Roberts <dan@adadevelopersacademy.org>
2017-03-21 23:07:59 -0700

    Make the Books controller pull data from ActiveRecord

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,12 @@
 class BooksController < ApplicationController
-  BOOKS = [{ title: "Hidden Figures", author: "Margot Lee Shetterly"},
-    { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-    { title: "Kindred", author: "Octavia E. Butler"}]
+
 
   def index
-    @books = BOOKS
+    @books = Book.all
   end
 
   def show
     id = params[:id].to_i
-    @book = BOOKS[id]
+    @book = Book.find(id)
   end
 end
