e9c65329ffe231271a4e32c95ccfa6f0456c81e8
Dan Roberts <dan@adadevelopersacademy.org>
2017-03-22 10:18:36 -0700

    Added new page with no form yet

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,9 @@
 class BooksController < ApplicationController
-
-
   def index
     @books = Book.all
+  end
+
+  def new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index', as: 'welcome'
 
   get 'books', to: 'books#index', as: 'books'
+  get 'books/new', to: 'books#new', as: 'new_book'
 
   get 'books/:id', to: 'books#show', as: 'book'
+
 end
