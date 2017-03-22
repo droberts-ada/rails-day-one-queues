34d0410b0de6812136251266dd556ff48a411d0f
Dan Roberts <dan@adadevelopersacademy.org>
2017-03-22 11:16:39 -0700

    Add books#create action, utilizing the form data to add a new book to the database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,21 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    puts ">>>> DPR: In BooksController#create"
+
+    book_data = book_params
+    puts book_data.to_hash
+    Book.create(book_data)
+  end
+
   def show
     id = params[:id].to_i
     @book = Book.find(id)
+  end
+
+private
+  def book_params
+    return params.require(:book).permit(:author, :title, :synopsis, :publication_year, :publication_city)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'books', to: 'books#index', as: 'books'
   get 'books/new', to: 'books#new', as: 'new_book'
+  post 'books', to: 'books#create'
 
   get 'books/:id', to: 'books#show', as: 'book'
 
