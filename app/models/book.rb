7bb5e11bf80923f5f00c5cdb485f2c829483f070
Dan Roberts <dan@adadevelopersacademy.org>
2017-04-04 11:45:00 -0700

    Add some validations to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true, length: { in: 5..25 }
+  validates :author, presence: { message: "bad author bad authr"}
+
+
   def age
     return nil if publication_year.nil?
     Date.today.year - publication_year
